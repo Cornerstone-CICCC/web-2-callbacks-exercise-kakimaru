a9b8c347dc38bca291d7ebf5896f182a9c44f3ab
kakimaru <memimei0306@gmail.com>
2024-07-24 08:50:54 +0900

    tried ex

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,43 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+  if (err) {
+    console.error(err);
+    return;
+  }
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        const fullName = `${data1} ${data2}`;
+        const age = `${data3}`;
+        const hobbies = JSON.parse(data4); // [ 'swimming', 'dancing', 'reading' ]
+        // console.log(hobbies);
+
+        let hobbiesText = "";
+
+        if(hobbies.length === 1) {
+          hobbiesText = hobbies[0];
+        } else if (hobbies.length >= 2) {
+          hobbiesText = hobbies.join(' and ');
+        }
+
+        // console.log(hobbiesText); // swimming and dancing and reading
+        console.log(
+          `${fullName} is ${age} years old and his hobbies are ${hobbiesText}.`
+        );
+      });
+    });
+  });
+});
